1d41dfc41bbaf87e8bc996d6df90ea61c136c944
brucexx <brucest0078@gmail.com>
2017-01-10 11:43:05 +0800

    1.0.7  2017.1.10 1.修复killChannel的问题 2.修复读取数据递归问题

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ mkdir output
 mvn clean package -Dmaven.test.skip=true
 
 cd target
-unzip -o heisenberg-server-1.0.6.zip
-cd heisenberg-server-1.0.6
+unzip -o heisenberg-server-1.0.7.zip
+cd heisenberg-server-1.0.7
 tar czf heisenberg-server.tgz *
 cd ../../
 
-cp target/heisenberg-server-1.0.6/heisenberg-server.tgz output/
+cp target/heisenberg-server-1.0.7/heisenberg-server.tgz output/
 rm -rf ./target
